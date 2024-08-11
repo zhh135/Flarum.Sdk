129a40b8805db888d0ee16c498c68ff4b24ed8b6
Jeff Zeng <zhh13950510580@outlook.com>
2024-08-11 22:33:52 +0800

    [WIP]

---
+++ Flarum.Api/ApiContracts/GetAllDiscussionsApi.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Flarum.Api.Bases;
+
+namespace Flarum.Api.ApiContracts
+{
+    public class GetAllDiscussionsApi : ApiBase<>
+    {
+    }
+
+    public 
+}
--- Flarum.Api/Program.cs
+++ Flarum.Api/Program.cs
@@ -4,11 +4,12 @@ using Flarum.Api.Bases;
 
 var handler = new FlarumApiHandler();
 var options = new FlarumApiHandlerOption();
-options.Url = "https://discuss.flarum.org/";
+options.Url = "https://community.wvbtech.com/";
 var result = await handler.RequestAsync<GetForumInfoRequest, GetForumInfoResponse, ErrorResultBase, GetForumInfoActualRequest>(new GetForumInfoApi(), options);
 var info = result.Match(
     success => success?.Data.flarumForum,
     error => new() { });
 Console.WriteLine($"{info.Title}");
-
+Console.WriteLine($"{info.Description}");
+Console.WriteLine($"{info.WelcomeMessage}");
 //Console.WriteLine($"title:{info.Title}");
