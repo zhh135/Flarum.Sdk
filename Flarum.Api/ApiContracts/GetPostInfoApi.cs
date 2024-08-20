7600937b2687b6f829c671d1a9bfd914aedb66f5
Jeff Zeng <zhh13950510580@outlook.com>
2024-08-20 22:32:36 +0800

    [WIP]

---
+++ Flarum.Api/ApiContracts/GetPostInfoApi.cs
@@ -0,0 +1,45 @@
+﻿using Flarum.Api.Bases;
+using Flarum.Api.Bases.ModelBases;
+using Flarum.Api.Models.ResponseModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Flarum.Api.ApiContracts
+{
+    public class GetPostInfoApi :
+        ApiBase<GetForumInfoActualRequest, GetForumInfoRequest, GetForumInfoResponse, ErrorResultBase>
+    {
+        public override string ApiPath => "/api";
+        public override HttpMethod Method => HttpMethod.Get;
+
+        public override Task MapRequest(GetForumInfoRequest? request)
+        {
+            return Task.CompletedTask;
+        }
+    }
+
+    public class GetPostInfoActualRequest : ActualRequestBase
+    {
+
+    }
+
+    public class GetPostInfoRequest : RequestBase
+    {
+
+    }
+
+    public class GetInfoResponse : ResponseBase
+    {
+        [JsonPropertyName("data")] public ForumData Data { get; set; }
+
+        public class ForumData : FlarumDataBase
+        {
+            [JsonPropertyName("attributes")] public FlarumForumDto flarumForum { get; set; }
+        }
+
+    }
+}
---
+++ Flarum.Api/Models/ResponseModel/FlarumPostDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Flarum.Api.Models.ResponseModel
+{
+    public class FlarumPostDto
+    {
+        [JsonPropertyName("number")] public string Number { get; set; }
+        [JsonPropertyName("createdAt")] public DateTime CreatedAt { get; set; }
+        [JsonPropertyName("contentType")] public string ContentType { get; set; }
+        [JsonPropertyName("contentHtml")] public string ContentHtml { get; set; }
+        [JsonPropertyName("renderFailed")] public bool RenderFailed { get; set; }
+        [JsonPropertyName("canEdit")] public bool CanEdit { get; set; }
+        [JsonPropertyName("canDelete")] public bool CanDelete { get; set; }
+        [JsonPropertyName("canHide")] public bool CanHide { get; set; }
+        [JsonPropertyName("canFlag")] public bool CanFlag { get; set; }
+        [JsonPropertyName("isApproved")] public bool IsApproved { get; set; }
+    }
+}
