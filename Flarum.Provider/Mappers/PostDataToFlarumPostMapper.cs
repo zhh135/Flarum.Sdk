31a81c3746a4de3234e56976ee1c75ca000a0e12
Jeff Zeng <zhh13950510580@outlook.com>
2024-08-25 14:24:04 +0800

    [WIP] [Feat]

---
+++ Flarum.Provider/Mappers/PostDataToFlarumPostMapper.cs
@@ -0,0 +1,40 @@
+﻿using Flarum.Provider.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flarum.Provider.Mappers
+{
+    public class PostDataToFlarumPostMapper
+    {
+        public static FlarumPost MapToFlarumPost(FlarumPost data)
+        {
+            return new FlarumPost
+            {
+                Number = data.Number,
+                CreatedAt = data.CreatedAt,
+                ContentType = data.ContentType,
+                ContentHtml = data.ContentHtml,
+                RenderFailed = data.RenderFailed,
+                CanEdit = data.CanEdit,
+                CanDelete = data.CanDelete,
+                CanHide = data.CanHide,
+                CanFlag = data.CanFlag,
+                IsApproved = data.IsApproved,
+                CanApprove = data.CanApprove,
+                CanReact = data.CanReact,
+                HasDownvoted = data.HasDownvoted,
+                HasUpvoted = data.HasUpvoted,
+                CanSeeVotes = data.CanSeeVotes,
+                Votes = data.Votes,
+                CanVote = data.CanVote,
+                SeeVoters = data.SeeVoters,
+                CanViewEditHistory = data.CanViewEditHistory,
+                CanDeleteEditHistory = data.CanDeleteEditHistory,
+                CanRollbackEditHistory = data.CanRollbackEditHistory
+            };
+        }
+    }
+}
---
+++ Flarum.Provider/Mappers/TagDataToFlarumTagMapper.cs
@@ -0,0 +1,25 @@
+﻿using Flarum.Provider.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flarum.Provider.Mappers
+{
+    public class TagDataToFlarumTagMapper
+    {
+        public static FlarumTag MapToFlarumTag(FlarumTag data)
+        {
+            return new FlarumTag
+            {
+                Name = data.Name,
+                Description = data.Description,
+                Slug = data.Slug,
+                Color = data.Color,
+                BackgroundUrl = data.BackgroundUrl,
+                BackgroundMode = data.BackgroundMode,
+            }
+        }
+    }
+}
